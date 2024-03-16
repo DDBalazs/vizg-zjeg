5d95a0edeb5e25b4f5ba5de489eba818a180a0f6
Balázs <Balzs@DESKTOP-ID885J2>
2024-03-16 10:57:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace vizgözjeg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem adja meg a víz hőmérsékletét: ");
+            int h = int.Parse(Console.ReadLine());
+            if (h <= 0)
+            {
+                Console.WriteLine("A víz jéggé fagyott");
+            }
+            else if (0 < h & h < 100)
+            {
+                Console.WriteLine("A víz folyékony");
+            }
+            else if (h >= 100)
+            {
+                Console.WriteLine("A vízből gőz lett");
+            }
+        }
+    }
+}
